4d41f9c37d1af9eeddc5054cd3b6156bd0a37612
Kelsi Flatland <kelsi.flatland@gmail.com>
2021-05-11 12:49:44 -0700

    Create App component.

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def homepage():
     """ Render app. """
 
-    return('hi')
+    return render_template('app.html')
 
 
 if __name__ == '__main__':
---
+++ static/js/app.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return(<h1>Hello World 🌍</h1>)
+}
+
+ReactDOM.render(<App />, document.querySelector('#app'));
